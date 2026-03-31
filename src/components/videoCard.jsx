62dc3a88fc70567c6cba5283ab8522393f79d0fe
raginimegala <ragini.megala@gmail.com>
2026-03-31 20:08:03 +0530

    pushing_after_1hour20_ mins

--- src/components/videoCard.jsx
+++ src/components/videoCard.jsx
@@ -2,10 +2,29 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { demoThumbnailUrl,demoVideoUrl,demoVideoTitle, demoChannelUrl,demoChannelTitle } from '../Utils/Constants'
 
-function VideoCard() {
+function VideoCard({video : {id:{videoId}, snippet}}) {
     return (
         <>
-        <div>VideoCard</div>
+        <div className='videoDetail'>
+            <a href="{videoId? '/video/${videoId}' : demoVideourl}">
+            <img src={snippet?.thumbnails?.high?.url} alt={snippet?.title}/></a>
+
+            <div>
+            <Link to="{videoId? '/video/${videoId}' : demoVideourl}"></Link>
+             </div>   
+
+        </div>
+        <div>
+           
+            <div>
+                {snippet.title?.slice(0,60) || demoVideoTitle.slice(0,60)}
+            </div>
+        <div>
+            <Link to="{snippet.channelId? '/channel/${snippet.channelId}' : demoChannelUrl}"></Link>
+            {snippet.channelTitle || demoChannelTitle}
+        
+            </div>
+        </div>    
 
         </>
     )
